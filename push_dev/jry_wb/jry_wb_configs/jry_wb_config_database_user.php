2100aea9d875ffc9c41be011a0fa71734018bc3c
火星大王 <lijunyandeyouxiang@163.com>
2019-03-11 13:22:42 +0800

    吔,操作失误，再次加入了配置文件

---
+++ push_dev/jry_wb/jry_wb_configs/jry_wb_config_database_user.php
@@ -0,0 +1,16 @@
+<?php
+	define('jry_wb_database_name'			,'');	//数据库名称
+	define('jry_wb_database_addr'			,'');		//数据库位置
+	define('jry_wb_database_user_name'		,'');		//用户名
+	define('jry_wb_database_user_password'	,'');		//密码
+	define('jry_wb_database_all_prefix'		,'');		//前缀
+	
+	if(!constant('jry_wb_host_switch'))					//分站控制
+	{
+		define('jry_wb_host_database_name'			,'');
+		define('jry_wb_host_database_addr'			,'');
+		define('jry_wb_host_database_user_name'		,'');
+		define('jry_wb_host_database_user_password'	,'');
+		define('jry_wb_host_database_all_prefix'	,'');	
+	}
+?>
