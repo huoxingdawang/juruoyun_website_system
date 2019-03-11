48f642aa0dc0aff5a38b8950d35af4c3d6f162c1
火星大王 <lijunyandeyouxiang@163.com>
2019-03-11 13:16:34 +0800

    test

--- push_dev/jry_wb/jry_wb_configs/jry_wb_config_mail_user.php
+++
@@ -1,8 +0,0 @@
-<?php
-	define('jry_wb_mail_pas'		,'');			//smtp密码
-	define('jry_wb_mail_user'		,'');//smtp用户
-	define('jry_wb_mail_host'		,'');	//smtp服务器
-	define('jry_wb_mail_to'			,'');//发件名称
-	define('jry_wb_mail_replay'		,constant('jry_wb_mail_user'));//回复用户
-	define('jry_wb_mail_replay_name','');//回复名称
-?>
