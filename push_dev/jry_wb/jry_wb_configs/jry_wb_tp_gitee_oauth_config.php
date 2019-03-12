4be919a4a76e1c63f23306054baa2a38b3760ae2
火星大王 <lijunyandeyouxiang@163.com>
2019-03-12 12:47:43 +0800

    更新码云oAuth配置文件

---
+++ push_dev/jry_wb/jry_wb_configs/jry_wb_tp_gitee_oauth_config.php
@@ -0,0 +1,5 @@
+<?php
+	include_once('jry_wb_config_default_system.php');
+	define('jry_wb_tp_gitee_oauth_config_client_id','');							//gitee oAuth client_id
+	define('jry_wb_tp_gitee_oauth_config_client_secret','');//gitee oAuth client_secret
+?>
