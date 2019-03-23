292ddcb90c09099178eb015d5f33c99889a4f2c1
火星大王 <lijunyandeyouxiang@163.com>
2019-03-23 20:29:23 +0800

    修复没有SharedWorker不运行的问题

--- dev/js/general_js/jry_wb_js_session.js
+++ dev/js/general_js/jry_wb_js_session.js
@@ -1,24 +1,34 @@
 var jry_wb_js_session=new function()
 {
 	var lastsend=0;
-	var worker=new SharedWorker(jry_wb_message.jry_wb_host+'jry_wb_js/sharedworker.js');
+	this.close=false;
 	var map=new Map();
-	worker.port.onmessage=function(data)
+	if(typeof SharedWorker=='undefined')
+		this.close=true;
+	else
 	{
-		data=data.data;
-		if(lastsend==data.key)
-			return;
-		var func=map.get(data.to);
-		if(typeof func=='function')
-			func(data.data);
-	};
-	worker.port.start();
+		var worker=new SharedWorker(jry_wb_message.jry_wb_host+'jry_wb_js/sharedworker.js');
+		worker.port.onmessage=function(data)
+		{
+			data=data.data;
+			if(lastsend==data.key)
+				return;
+			var func=map.get(data.to);
+			if(typeof func=='function')
+				func(data.data);
+		};
+		worker.port.start();
+	}
 	this.send=function(to,data)
 	{
+		if(this.close)
+			return;
 		worker.port.postMessage({'to':to,'data':data,'key':(lastsend=Math.random())});
 	};
 	this.add_listener=function(id,func)
 	{
+		if(this.close)
+			return;
 		map.set(id,func);
 	};
 };
