a2407527b056ab61195701ece8ec99f6f9313ed5
火星大王 <lijunyandeyouxiang@163.com>
2019-03-24 13:01:17 +0800

    接入了ip2region

--- push_dev/jry_wb/tools/jry_wb_get_ip_address.php
+++ push_dev/jry_wb/tools/jry_wb_get_ip_address.php
@@ -13,7 +13,22 @@
 			$json->by='db';
 			return $json;
 		}
-			
+		require '../jry_wb_tp_sdk/ip2region/Ip2Region.php';
+		$ip2region = new Ip2Region();
+		$json=explode('|',$ip2region->btreeSearch($ip)['region']);
+		if(!($json[4]==''&&$json[3]==''&&$json[2]==''&&$json[1]==''&&$json[0]==''))
+		{
+			$json='{"by":"ip2region","code":0,"data":{"ip":"'.$ip.'","isp":"'.$json[4].'","area":"","city":"'.$json[3].'","county":"XX","isp_id":"","region":"'.$json[2].'","area_id":"","city_id":"","country":"'.$json[0].'","county_id":"xx","region_id":"","country_id":""}}';
+			$st = $conn->prepare('INSERT INTO '.constant('jry_wb_database_general').'ip (`ip`,`data`) VALUES (?,?)');
+			$st->bindParam(1,$ip);
+			$st->bindParam(2,$json);
+			$st->execute();
+			return json_decode($json);
+		}
+		else
+		{
+			return false;
+		}
 /*		$json=file_get_contents('http://ip.taobao.com/service/getIpInfo.php?ip='.$ip);
 		if($json=='')
 			$json='{"code":0,"data":{"country":"XX","area":"","region":"XX","city":"unknow","county":"unknow","isp":"unknow","country_id":"xx","area_id":"","region_id":"xx","city_id":"local","county_id":"local","isp_id":"local"}}';
@@ -30,7 +45,7 @@
 				$st->execute();
 			}
 		}
-		return json_decode($json);*/
+		return json_decode($json);
 		$json=json_decode(file_get_contents('http://freeapi.ipip.net/'.$ip));
 		if($json[4]==''&&$json[3]==''&&$json[2]==''&&$json[1]==''&&$json[0]=='')
 			return null;
@@ -39,7 +54,7 @@
 		$st->bindParam(1,$ip);
 		$st->bindParam(2,$json);
 		$st->execute();		
-		return json_decode($json);
+		return json_decode($json);*/
 	}
 	if(($_SERVER['DOCUMENT_ROOT'].$_SERVER['PHP_SELF'])==__FILE__)
 		echo json_encode(jry_wb_get_ip_address($_GET['ip']));
