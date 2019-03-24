81461f1cdf4974ddc6ab8daef71ffdb0fd7e98b5
火星大王 <lijunyandeyouxiang@163.com>
2019-03-24 13:13:31 +0800

    优化了ip查询

--- push_dev/jry_wb/tools/jry_wb_get_ip_address.php
+++ push_dev/jry_wb/tools/jry_wb_get_ip_address.php
@@ -1,5 +1,6 @@
 <?php
 	include_once("jry_wb_includes.php");
+	require_once '../jry_wb_tp_sdk/ip2region/Ip2Region.php';
 	function jry_wb_get_ip_address($ip)
 	{
 		$conn=jry_wb_connect_database();
@@ -13,22 +14,10 @@
 			$json->by='db';
 			return $json;
 		}
-		require '../jry_wb_tp_sdk/ip2region/Ip2Region.php';
 		$ip2region = new Ip2Region();
 		$json=explode('|',$ip2region->btreeSearch($ip)['region']);
 		if(!($json[4]==''&&$json[3]==''&&$json[2]==''&&$json[1]==''&&$json[0]==''))
-		{
-			$json='{"by":"ip2region","code":0,"data":{"ip":"'.$ip.'","isp":"'.$json[4].'","area":"","city":"'.$json[3].'","county":"XX","isp_id":"","region":"'.$json[2].'","area_id":"","city_id":"","country":"'.$json[0].'","county_id":"xx","region_id":"","country_id":""}}';
-			$st = $conn->prepare('INSERT INTO '.constant('jry_wb_database_general').'ip (`ip`,`data`) VALUES (?,?)');
-			$st->bindParam(1,$ip);
-			$st->bindParam(2,$json);
-			$st->execute();
-			return json_decode($json);
-		}
-		else
-		{
-			return false;
-		}
+			return json_decode('{"by":"ip2region","code":0,"data":{"ip":"'.$ip.'","isp":"'.$json[4].'","area":"","city":"'.$json[3].'","county":"XX","isp_id":"","region":"'.$json[2].'","area_id":"","city_id":"","country":"'.$json[0].'","county_id":"xx","region_id":"","country_id":""}}');
 /*		$json=file_get_contents('http://ip.taobao.com/service/getIpInfo.php?ip='.$ip);
 		if($json=='')
 			$json='{"code":0,"data":{"country":"XX","area":"","region":"XX","city":"unknow","county":"unknow","isp":"unknow","country_id":"xx","area_id":"","region_id":"xx","city_id":"local","county_id":"local","isp_id":"local"}}';
