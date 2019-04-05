20a68c0b021472066d9a050dce113d3dbba19454
火星大王 <lijunyandeyouxiang@163.com>
2019-04-05 18:09:25 +0800

    修复了某些情况下不能设置文件上传完毕的问题

--- push_dev/jry_wb/jry_wb_netdisk/jry_nd_database_set_file_ok.php
+++ push_dev/jry_wb/jry_wb_netdisk/jry_nd_database_set_file_ok.php
@@ -2,11 +2,10 @@
 	include_once('jry_nd_database_include.php');
 	function jry_nd_database_set_file_ok($conn,$user,$file_id,$size)
 	{
-		$st = $conn->prepare('UPDATE '.constant('jry_wb_netdisk').'file_list SET uploading=? , size=? WHERE `file_id`=? AND id=?');
-		$st->bindValue(1,0);
-		$st->bindValue(2,$size);
-		$st->bindValue(3,$file_id);
-		$st->bindValue(4,$user['id']);
+		$st = $conn->prepare('UPDATE '.constant('jry_wb_netdisk').'file_list SET uploading=0 , size=? WHERE `file_id`=? AND id=?');
+		$st->bindValue(1,$size);
+		$st->bindValue(2,$file_id);
+		$st->bindValue(3,$user['id']);
 		$st->execute();
 	}				
 ?>
