5b2b8c73775d3acffa117bb00cab6781fabde959
火星大王 <lijunyandeyouxiang@163.com>
2019-04-13 23:41:44 +0800

    删掉了一些文件

--- dev/oj/auto_fix.cpp
+++
@@ -1,8 +0,0 @@
-#include <cstdio>
-using namespace std;
-int main()
-{
-	freopen("out.sql","w",stdout);
-	for(int i=1;i<=644;i++)
-		printf("update oj_questionlist set oj_questionlist.right = (select COUNT(1) from oj_logs where oj_logs.result='right' AND oj_logs.ojquestionid=%d) where oj_questionlist.ojquestionid=%d;\nupdate oj_questionlist set oj_questionlist.submit = (select COUNT(1) from oj_logs where oj_logs.ojquestionid=%d)where oj_questionlist.ojquestionid=%d;\n",i,i,i,i);
-}
