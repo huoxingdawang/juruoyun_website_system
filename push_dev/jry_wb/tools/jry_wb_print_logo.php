ec0d072ad36a3af2313b7c4600220b3c18e25a0e
火星大王 <lijunyandeyouxiang@163.com>
2019-04-13 23:00:05 +0800

    自动输出返回主页的返回字样

--- push_dev/jry_wb/tools/jry_wb_print_logo.php
+++ push_dev/jry_wb/tools/jry_wb_print_logo.php
@@ -9,6 +9,6 @@
 		$i=0;
 		for(;$jry_wb_website_map[$i]['name']!='home';$i++);
 		$website=$jry_wb_website_map[$i];
-		echo '<a href="'.constant("jry_wb_host").$website['url'].'" target="_parent" class='.($active?'active':'').'>'.$website['show_name'].constant('jry_wb_name').'<img width="'.$width.'px" src="'.constant('jry_wb_logo_picture_address').'"></img></a>';
+		echo '<a href="'.constant("jry_wb_host").$website['url'].'" target="_parent" class='.($active?'active':'').'>'.($active?'':'返回').$website['show_name'].constant('jry_wb_name').'<img width="'.$width.'px" src="'.constant('jry_wb_logo_picture_address').'"></img></a>';
 	}
 ?>
