9c93954ecf9222813bf49918a6e28bffca584aad
火星大王 <lijunyandeyouxiang@163.com>
2019-04-16 13:13:16 +0800

    修复了身份证最后一位是X判断错误的问题

--- dev/js/general_js/jry_wb_test_china_id_card.js
+++ dev/js/general_js/jry_wb_test_china_id_card.js
@@ -8,22 +8,26 @@ function jry_wb_test_china_id_card(idcard)
 	else
         return false;
 }   
-function jry_wb_test_china_id_card_18(idcard)
+function jry_wb_test_china_id_card_18(idcardd)
 {   
-	idcard.toUpperCase();
+	idcardd=idcardd.toUpperCase();
 	var wi = [7,9,10,5,8,4,2,1,6,3,7,9,10,5,8,4,2,1];
 	var valideCode = [1,0,10,9,8,7,6,5,4,3,2];
-    var sum = 0;
-    if (idcard[17].toUpperCase()=='X')
-        idcard[17] = 10;
+	var sum = 0;
+	var idcard=[];
+	for(var i=0;i<idcardd.length;i++)
+		if (idcardd[i].toUpperCase()=='X')
+			idcard[i] = 10;
+		else
+			idcard[i]=parseInt(idcardd[i]);
     for ( var i = 0; i < 17; i++)
         sum += wi[i] * idcard[i]; 
     valCodePosition = sum % 11;  
     if (idcard[17] == valideCode[valCodePosition])
 	{
-		var year =  idcard.substring(6,10);   
-		var month = idcard.substring(10,12);   
-		var day = idcard.substring(12,14);   
+		var year =  idcardd.substring(6,10);   
+		var month = idcardd.substring(10,12);   
+		var day = idcardd.substring(12,14);   
 		var temp_date = new Date(year,parseFloat(month)-1,parseFloat(day));   
 		if(temp_date.getFullYear()!=parseFloat(year)||temp_date.getMonth()!=parseFloat(month)-1||temp_date.getDate()!=parseFloat(day)) 
 			return false;   
