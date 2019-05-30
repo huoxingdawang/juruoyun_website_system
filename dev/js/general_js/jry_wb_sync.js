0d020b92c100b16a9a36bcceb24d41f6de235e11
火星大王 <lijunyandeyouxiang@163.com>
2019-05-30 16:13:19 +0800

    暴力防止数组为空sync的报错

--- dev/js/general_js/jry_wb_sync.js
+++ dev/js/general_js/jry_wb_sync.js
@@ -5,19 +5,20 @@ function jry_wb_sync_data_with_server(syncname,dataurl,array,cmp,after,sort_cmp)
 		jry_wb_loading_off();	
 		var data = JSON.parse(data);
 		var newdata=false;
-		if(((data==null||typeof data.length=='number')&&data.length>0)||(typeof data.length=='undefined'&&typeof data.data!='undefined'&&data.data.length>0))
+		if(data==null)
+			data=[];
+		if((typeof data.length=='number'&&data.length>0)||(typeof data.length=='undefined'&&typeof data.data!='undefined'&&data.data.length>0))
 			newdata=true;
-		if(data!=null)
-			if(typeof data.code!='undefined'&&data.code==false)
-			{
-				if(data.reason==100000)
-					jry_wb_beautiful_alert.alert("没有登录","","window.location.href=''");
-				else if(data.reason==100001)
-					jry_wb_beautiful_alert.alert("权限缺失","缺少"+data.extern,"window.location.href=''");
-				else
-					jry_wb_beautiful_alert.alert("错误"+data.reason,"请联系开发组");
-				return ;	
-			}
+		if(typeof data.code!='undefined'&&data.code==false)
+		{
+			if(data.reason==100000)
+				jry_wb_beautiful_alert.alert("没有登录","","window.location.href=''");
+			else if(data.reason==100001)
+				jry_wb_beautiful_alert.alert("权限缺失","缺少"+data.extern,"window.location.href=''");
+			else
+				jry_wb_beautiful_alert.alert("错误"+data.reason,"请联系开发组");
+			return ;	
+		}
 		if(data==null||typeof data.length=='number')
 			after(jry_wb_sync_data_with_array(syncname,data,cmp,sort_cmp),newdata);
 		else if(typeof data.length=='undefined')
