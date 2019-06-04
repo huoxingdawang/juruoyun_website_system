9d5c10381da8d339c6c6445e8e1cdefabb2244c8
火星大王 <lijunyandeyouxiang@163.com>
2019-06-04 16:29:51 +0800

    加入后台输出连接时输出id的功能

--- push_dev/jry_wb/tools/jry_wb_print_href.php
+++ push_dev/jry_wb/tools/jry_wb_print_href.php
@@ -1,6 +1,6 @@
 <?php
 	include_once("jry_wb_includes.php");
-	function jry_wb_print_href($name,$css="",$zhuijia="",$return=false)
+	function jry_wb_print_href($name,$css="",$zhuijia="",$return=false,$id='')
 	{
 		global $jry_wb_website_map;
 		jry_wb_load_website_map();
@@ -12,17 +12,17 @@
 			if($website['type']==1)
 			{
 				$add=$website[url].$zhuijia;
-				echo "<a href=".$add."  target='_blank' class=".$css.">".$website['show_name']."</a>";
+				echo '<a id="'.$id.'" href='.$add.' target="_blank" class='.$css.'>'.$website['show_name'].'</a>';
 			}
 			if($website['type']==2)
 			{
 				$add=$website[url].$zhuijia;
-				echo "<a href=".$add."  target='_parent' class=".$css.">".$website['show_name']."</a>";
+				echo '<a id="'.$id.'" href='.$add.' target="_parent" class='.$css.'>'.$website['show_name'].'</a>';
 			}
 			if($website['type']==0)
 			{
 				$add=constant("jry_wb_host").$website[url].$zhuijia;
-				echo "<a href=".$add."  target='_parent' class=".$css.">".$website['show_name']."</a>";
+				echo '<a id="'.$id.'" href='.$add.' target="_parent" class='.$css.'>'.$website['show_name'].'</a>';
 			}
 		}
 		else
