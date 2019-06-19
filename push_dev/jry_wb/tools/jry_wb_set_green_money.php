a58266d9f6533d539f8078e523028cb1de23fffd
火星大王 <lijunyandeyouxiang@163.com>
2019-06-19 21:00:52 +0800

    修复绿币操作log写入错误的问题，修复登录奖励绿币时间错误更新的问题

--- push_dev/jry_wb/tools/jry_wb_set_green_money.php
+++ push_dev/jry_wb/tools/jry_wb_set_green_money.php
@@ -3,13 +3,13 @@
 	define('jry_wb_log_type_green_money_login_add',0);
 	define('jry_wb_log_type_green_money_pay_nd_size',1);
 	define('jry_wb_log_type_green_money_pay_nd_fast_size',2);
-	
+	define('jry_wb_log_type_green_money_invite_user',3);
 	function jry_wb_set_green_money($conn,&$user,$money,$by)
 	{
 		if($user['green_money']+$money<0)
 			return false;
-		jry_wb_echo_log(constant('jry_wb_log_type_green_money'),array('money'=>$money,'by'=>$by));
-		$st = $conn->prepare('UPDATE '.JRY_WB_DATABASE_GENERAL.'users set greendate=?, green_money=green_money+?,lasttime=? where id=? ');
+		jry_wb_echo_log(constant('jry_wb_log_type_green_money'),array('money'=>$money,'by'=>$by),$user['id']);
+		$st = $conn->prepare('UPDATE '.JRY_WB_DATABASE_GENERAL.'users set greendate=GREATEST(greendate,?), green_money=green_money+?,lasttime=? where id=? ');
 		if(constant('jry_wb_log_type_green_money_login_add')==$by)
 			$st->bindParam(1,$user['greendate']=jry_wb_get_time());
 		else
