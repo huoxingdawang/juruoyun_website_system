5e1337a5309270cf6e691f75b9d3fba81b4d3161
火星大王 <lijunyandeyouxiang@163.com>
2019-07-26 12:30:48 +0800

    修复一个报错

--- push_dev/jry_wb/jry_wb_manage_system/jry_wb_manage_user_get_information.php
+++ push_dev/jry_wb/jry_wb_manage_system/jry_wb_manage_user_get_information.php
@@ -7,13 +7,13 @@
 		$conn2=jry_wb_connect_database();
 		$st = $conn2->prepare('UPDATE '.JRY_WB_DATABASE_GENERAL.'users SET `order`=(SELECT MIN(`order`) FROM '.JRY_WB_DATABASE_MANAGE_SYSTEM.'competence WHERE `type` IN (SUBSTRING_INDEX(SUBSTRING(JSON_UNQUOTE('.JRY_WB_DATABASE_GENERAL_PREFIX.'users.type),2),\']\',1)))');
 		$st->execute();
-		$st = $conn2->prepare('SELECT `id`,`name`,`use` FROM '.JRY_WB_DATABASE_GENERAL.'users WHERE `lasttime`>? AND `order`>?');
+		$st = $conn2->prepare('SELECT `id`,`name`,`use`,`lasttime` FROM '.JRY_WB_DATABASE_GENERAL.'users WHERE `lasttime`>? AND `order`>?');
 		$st->bindValue(1,urldecode($_GET['lasttime']));
 		$st->bindValue(2,$jry_wb_login_user['order']);
 		$st->execute();
 		$json=array();
 		foreach($st->fetchAll()as $users)
-			$json[]=array('id'=>(int)$users['id'],'use'=>$users['use'],'name'=>$users['name']);
+			$json[]=array('id'=>(int)$users['id'],'use'=>$users['use'],'name'=>$users['name'],'lasttime'=>$users['lasttime']);
 		echo json_encode(array('code'=>true,'data'=>$json));
 	}
 	
