7dd74b48358188cf30f06acf1e4433a1adbcd4f9
火星大王 <lijunyandeyouxiang@163.com>
2019-07-28 10:05:46 +0800

    加入数据库查询的loadingcount

--- dev/js/general_js/jry_wb_indexeddb.js
+++ dev/js/general_js/jry_wb_indexeddb.js
@@ -36,17 +36,17 @@ function jry_wb_indexeddb_init()
 			jry_wb_beautiful_right_alert.alert('思维升级完成!',1000,'auto','ok');			
 		window.onindexeddbopen();
 	};
+	var creat_list=[{'name':'user'				,'key':'id'}			,{'name':'manage_user'				,'key':'id'}			,{'name':'lasttime'		,'key':'key'}			,{'name':'invitecode'		,'key':'incite_code_id'}	,{'name':'log'				,'key':'log_id'},
+					{'name':'oj_question_list'	,'key':'question_id'}	,{'name':'oj_manage_question_list'	,'key':'question_id'}	,{'name':'oj_logs'		,'key':'log_id'}		,{'name':'oj_classes'		,'key':'class_id'}			,{'name':'oj_error'			,'key':'error_id'},
+					{'name':'blog_list'			,'key':'blog_id'}		,{'name':'blog_draft_list'			,'key':'blog_id'}		,{'name':'chat_rooms'	,'key':'chat_room_id'}	,{'name':'chat_messages'	,'key':'chat_text_id'}		,{'name':'nd_group'			,'key':'group_id'},
+					{'name':'nd_area'			,'key':'area_id'}		,{'name':'nd_file_list'				,'key':'file_id'}		,{'name':'nd_share_list','key':'share_id'}		,{'name':'manage_user_list'	,'key':'id'}				,{'name':'manage_competence','key':'type'},
+					{'name':'manage_bigdeal'	,'key':'bigdeal_id'}	,{'name':'manage_hengfu'			,'key':'hengfu_id'}		,{'name':'manage_tanmu'	,'key':'tanmu_id'}		,{'name':'qq_music'			,'key':'mid'}				,{'name':'163_music'		,'key':'mid'},
+					{'name':'songlist'			,'key':'slid'}			,{'name':'blog_text'				,'key':'blog_id'}		,{'name':'blog_draft_text'	,'key':'blog_id'}	,{'name':'ip'	,'key':'data.ip'}];
 	request.onupgradeneeded=function(event)
 	{
 		update_flag=true;
 		jry_wb_beautiful_right_alert.alert('思维升级中......',1000,'auto','warn');
 		jry_wb_indexeddb=request.result;
-		var creat_list=[{'name':'user'				,'key':'id'}			,{'name':'manage_user'				,'key':'id'}			,{'name':'lasttime'		,'key':'key'}			,{'name':'invitecode'		,'key':'incite_code_id'}	,{'name':'log'				,'key':'log_id'},
-						{'name':'oj_question_list'	,'key':'question_id'}	,{'name':'oj_manage_question_list'	,'key':'question_id'}	,{'name':'oj_logs'		,'key':'log_id'}		,{'name':'oj_classes'		,'key':'class_id'}			,{'name':'oj_error'			,'key':'error_id'},
-						{'name':'blog_list'			,'key':'blog_id'}		,{'name':'blog_draft_list'			,'key':'blog_id'}		,{'name':'chat_rooms'	,'key':'chat_room_id'}	,{'name':'chat_messages'	,'key':'chat_text_id'}		,{'name':'nd_group'			,'key':'group_id'},
-						{'name':'nd_area'			,'key':'area_id'}		,{'name':'nd_file_list'				,'key':'file_id'}		,{'name':'nd_share_list','key':'share_id'}		,{'name':'manage_user_list'	,'key':'id'}				,{'name':'manage_competence','key':'type'},
-						{'name':'manage_bigdeal'	,'key':'bigdeal_id'}	,{'name':'manage_hengfu'			,'key':'hengfu_id'}		,{'name':'manage_tanmu'	,'key':'tanmu_id'}		,{'name':'qq_music'			,'key':'mid'}				,{'name':'163_music'		,'key':'mid'},
-						{'name':'songlist'			,'key':'slid'}			,{'name':'blog_text'				,'key':'blog_id'}		,{'name':'blog_draft_text'	,'key':'blog_id'}	,{'name':'ip'	,'key':'data.ip'}];
 		for(var i=0,n=creat_list.length;i<n;i++)
 			if(!jry_wb_indexeddb.objectStoreNames.contains(creat_list[i].name)) 
 				jry_wb_indexeddb.createObjectStore(creat_list[i].name,{keyPath:creat_list[i].key});
@@ -72,6 +72,7 @@ function jry_wb_indexeddb_get_lasttime(key,callback)
 }
 function jry_wb_indexeddb_get_all(db_name,callback)
 {
+	jry_wb_loading_on();
 	jry_wb_add_on_indexeddb_open(function()
 	{	
 		var re=jry_wb_indexeddb.transaction([db_name],'readwrite').objectStore(db_name).openCursor();
@@ -82,7 +83,7 @@ function jry_wb_indexeddb_get_all(db_name,callback)
 			if (cursor)
 				data.push(cursor.value),cursor.continue();
 			else
-				callback(data);
+				jry_wb_loading_off(),callback(data);
 		};
 	});	
 }
