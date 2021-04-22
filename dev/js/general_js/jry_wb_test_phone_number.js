ccb6e117ba9a682a519f30c9303ae285d3033111
火星大王 <lijunyandeyouxiang@163.com>
2021-04-22 21:07:36 +0800

    支持新版手机号

--- dev/js/general_js/jry_wb_test_phone_number.js
+++ dev/js/general_js/jry_wb_test_phone_number.js
@@ -1,6 +1,6 @@
 function jry_wb_test_phone_number( phone)
 { 
-    if(!(/^1[34578]\d{9}$/.test(phone)))
+    if(!(/^[1](([3][0-9])|([4][5-9])|([5][0-3,5-9])|([6][5,6])|([7][0-8])|([8][0-9])|([9][1,8,9]))[0-9]{8}$/.test(phone)))
         return false; 
 	return true;
 }
